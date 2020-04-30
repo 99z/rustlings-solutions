d48223026127affaed23ab93fcfd92388949de24
Nicolas S. Montanaro <mail@nicolasmontanaro.com>
2020-04-29 23:28:26 -0400

    done

---
+++ functions/functions1.rs
@@ -0,0 +1,11 @@
+// functions1.rs
+// Make me compile! Execute `rustlings hint functions1` for hints :)
+
+
+fn call_me() -> usize {
+    return 1;
+}
+
+fn main() {
+    call_me();
+}
---
+++ functions/functions4.rs
@@ -0,0 +1,24 @@
+// functions4.rs
+// Make me compile! Execute `rustlings hint functions4` for hints :)
+
+// This store is having a sale where if the price is an even number, you get
+// 10 Rustbucks off, but if it's an odd number, it's 3 Rustbucks off.
+
+
+
+fn main() {
+    let original_price = 51;
+    println!("Your sale price is {}", sale_price(original_price));
+}
+
+fn sale_price(price: u8) -> u8 {
+    if is_even(price) {
+        price - 10
+    } else {
+        price - 3
+    }
+}
+
+fn is_even(num: u8) -> bool {
+    num % 2 == 0
+}
